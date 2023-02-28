482c38aa25f68d3411b6fc61338c11400f8bf68a
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-02-28 17:44:56 +0900

    Lesson15

---
+++ src/main/java/com/techacademy/indexController.java
@@ -0,0 +1,22 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class indexController {
+
+	@Controller
+	public class IndexController {
+
+
+		@GetMapping("/")
+		public String getIndex() {
+			// index.htmlに画面遷移
+			return "index";
+		}
+	}
+
+		}
+
+
+
